3542108184d4d76c56fc7585f15e10f9c60f5dc8
schloing <user.datagram.protocol@proton.me>
2023-07-23 11:00:10 +1000

    basic capture-driven bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,41 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// example bot de-verbosed using linq
+// 1) plays capture moves if possible
+// 2) plays mate-in-one, if it sees one
+// 3) plays the first move it sees otherwise
 
 public class MyBot : IChessBot
 {
+    // pawn, knight, bishop, rook, queen, king
+    private readonly int[] PieceValues = { 1, 3, 3, 5, 9, 999 };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] legalMoves = board.GetLegalMoves();
+
+        List<Move> capturelegalMoves = legalMoves.Where(move => move.IsCapture).ToList();
+
+        if (capturelegalMoves.Count == 0)
+            return legalMoves[0];
+
+        Move bestCaptureMove = capturelegalMoves
+            .OrderByDescending(
+                move => PieceValues[(int)move.CapturePieceType]
+                * (MoveIsCheckmate(board, move) ? 100 : 1))
+            .FirstOrDefault();
+
+        return bestCaptureMove;
+    }
+
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
